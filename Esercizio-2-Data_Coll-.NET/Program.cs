c298eed9636e42c731201b9f49de65a662508e6a
emifa <emifa@LAPTOP-1TM449QB>
2023-06-28 17:00:14 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-2-Data_Coll-.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+
+ArrayList arrayList = new ArrayList();
+arrayList.Add("ciao");
+arrayList.Add(0);
+arrayList.Add(10.14);
+arrayList.Add(true);
+Print(arrayList);
+
+ static void Print(IEnumerable list)
+{
+    foreach(var item in list )
+        Console.WriteLine(item);
+}
